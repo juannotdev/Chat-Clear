4e4e5a79b6119f9d05bdd4edc0404cb31530ab50
juan <109624079+juannotdev@users.noreply.github.com>
2022-08-12 19:37:25 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+description 'FiveM clear chat for both server sided, and client.'
+version 'github.com/juannotdev'
+
+server_scripts {
+  'server/server.lua'
+} 
